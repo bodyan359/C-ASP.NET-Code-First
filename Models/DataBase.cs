bd4da728185bbb078987462e7e524f223a6a3bbb
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2019-05-29 22:16:38 +0200

    Add project files.

---
+++ Models/DataBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class DataBase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=db-mssql;Initial Catalog=s15526;Integrated Security=True");
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Animal> Animals {get; set;}
+    }
+}
